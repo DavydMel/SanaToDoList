32f9671728743183b8370e5fa89754e46d61bb56
Davyd Melnychenko <ipz204_mdv@student.ztu.edu.ua>
2023-05-29 00:04:41 +0300

    added complete, delete task ability

--- todolist-react/src/components/ToDoListApp/ToDoListItem.tsx
+++ todolist-react/src/components/ToDoListApp/ToDoListItem.tsx
@@ -1,7 +1,9 @@
 import ListGroup from 'react-bootstrap/ListGroup';
+import Button from "react-bootstrap/Button";
 import {ToDoItem} from "../../models/ToDoItem";
 import {Category} from "../../models/Category";
 import {useAppDispatch} from "../../features/hooks";
+import {completetodoitem, deletetodoitem} from "../../redux/todolistSlice";
 
 interface ToDoListItemProps {
     item: ToDoItem,
@@ -22,6 +24,13 @@ function ToDoListItem({item, category}: ToDoListItemProps) {
         bgColor = "danger"
     }
 
+    function handleComplete() {
+        dispatch(completetodoitem(item.id));
+    }
+    function handleDelete() {
+        dispatch(deletetodoitem(item.id));
+    }
+
     return (
         <ListGroup.Item variant={bgColor}>
             <div className="d-flex flex-column">
@@ -30,6 +39,21 @@ function ToDoListItem({item, category}: ToDoListItemProps) {
                     {item.deadline? <small>{item.deadline}</small> : ""}
                 </div>
                 <p className="mb-1 align-self-start">Category: {category.name}</p>
+                <div className="mt-3">
+                    <Button
+                        className="w-25 mx-2"
+                        onClick={handleComplete}
+                    >
+                        {item.is_completed? "Don't complete" :"Complete"}
+                    </Button>
+                    <Button
+                        className="w-25 mx-2"
+                        variant="danger"
+                        onClick={handleDelete}
+                    >
+                        Delete
+                    </Button>
+                </div>
             </div>
         </ListGroup.Item>
     );
--- todolist-react/src/redux/todolistSlice.ts
+++ todolist-react/src/redux/todolistSlice.ts
@@ -17,9 +17,29 @@ export const todolistSlice = createSlice({
         addtodoitem: (state,
                       action: PayloadAction<ToDoItem>) => {
             state.data.ToDoItems = [...state.data.ToDoItems, action.payload];
+        },
+        completetodoitem: (state,
+                           action: PayloadAction<number>) => {
+            let todoitem =
+                state.data.ToDoItems.find(i => i.id === action.payload);
+            if (todoitem) {
+                todoitem.is_completed = !todoitem.is_completed;
+            }
+        },
+        deletetodoitem: (state,
+            action: PayloadAction<number>) => {
+            let index = state.data.ToDoItems
+                .findIndex(i => i.id === action.payload);
+            if (index > -1) {
+                state.data.ToDoItems.splice(index, 1);
+            }
         }
     }
 });
 
-export const { addtodoitem } = todolistSlice.actions;
+export const {
+    addtodoitem,
+    completetodoitem,
+    deletetodoitem
+} = todolistSlice.actions;
 export default todolistSlice.reducer;
